d4a392381e4a65b2c603f38e5b22fe0b665794ca
doremansunio <136595579+doremansunio@users.noreply.github.com>
2024-02-15 16:04:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,3 +19,26 @@ resource "rafay_group" "group-Workspace" {
   name        = "WrkspAdmin-grp-${var.project_name}"
   description = "Workspace Admin Group for ${var.project_name}"  
 }
+
+#
+resource "rafay_namespace_network_policy_rule" "demo-withinworkspacerule" {
+  depends_on = [time_sleep.wait_30_seconds]
+  metadata {    
+    name    = var.network_policy_rule_name
+    project = var.project_name    
+  }
+  spec {
+    artifact {
+      type = "Yaml"
+      artifact {               
+        paths {                               
+          name = "file://${path.module}/netfiles/${var.project_name}-within-ws-rule.yaml"        
+        } 
+      }
+    }
+    version = var.network_policy_rule_version
+    sharing {
+      enabled = false
+    }
+  }
+}
