43f62fcb0488af4b9b9d034197628af06e3ef994
doremansunio <136595579+doremansunio@users.noreply.github.com>
2024-02-15 15:57:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,8 +15,7 @@ provider "rafay" {
   # provider_config_file = "./rafay_config.json"
 }
 
-resource "rafay_group" "group-Workspace" {
-  depends_on = [rafay_project.rafay_proj_new]
+resource "rafay_group" "group-Workspace" {  
   name        = "WrkspAdmin-grp-${var.project_name}"
   description = "Workspace Admin Group for ${var.project_name}"  
 }
